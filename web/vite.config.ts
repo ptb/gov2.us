15e4e429b951d1f57f457b130c26a440f50a969b
Peter T Bosse II <peter@ptb.dev>
2024-05-02 17:27:59 -0400

    Update deployment configuration in .github/workflows/deploy.yml

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,7 +5,13 @@ import { default as tsPathsPlugin } from "vite-tsconfig-paths"
 export default defineConfig({
   build: {
     emptyOutDir: true,
-    outDir: "../www"
+    outDir: "../www",
+    rollupOptions: {
+      output: {
+        manualChunks: (id) =>
+          id.includes("node_modules") ? "share" : undefined
+      }
+    }
   },
   plugins: [tsPathsPlugin(), reactPlugin()]
 })
